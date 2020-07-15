f4423430eaaeba9155203f278278197d0900a45c
nikitaBelobrov <nik.belobrov2018@gmail.com>
2020-07-15 13:22:33 +0300

    Try to write text to file

---
+++ byte_of_python/using_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+poem = '''\
+Программировать весело.
+Если работа скучна,
+Чтобы придать ей веселый тон -
+    используй Python!
+'''
+
+f = open('poem.txt', 'w')
+f.write(poem)
+f.close()
+
+f = open('poem.txt')
+
+while True:
+    line = f.readline()
+    if len(line) == 0:
+        break
+    print(line, end='')
+
+f.close()
